1827063d664b226749ac051a3fd472694db8a369
Maarten Beeckmans <maartenb@inuits.eu>
2020-09-09 22:29:13 +0200

    add puppetdb support

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,22 +3,31 @@
 #
 class profile_puppetmaster (
   Boolean         $autosign          = false,
-  Array['String'] $autosign_entries  = [],
+  Array[String]   $autosign_entries  = [],
   Boolean         $setup_foreman     = false,
   String          $version           = 'latest',
   Boolean         $setup_puppetdb    = false,
   Boolean         $setup_puppetboard = false,
 ) {
   # @TODO implement prometheus/graphite metrics
-  class { 'puppet::server':
-    autosign         => $autosign,
-    autosign_entries => $autosign_entries,
-    foreman          => $setup_foreman,
-    version          => $version,
-    multithreaded    => true,
-  }
   if $setup_puppetdb {
+    $server_storeconfigs = true
+    $server_reports = 'puppetdb'
     include profile_puppetmaster::puppetdb
+  } else {
+    $server_storeconfigs = false
+    $server_reports = 'foreman'
+  }
+  class { 'puppet':
+    server               => true,
+    autosign             => $autosign,
+    autosign_entries     => $autosign_entries,
+    server_foreman       => $setup_foreman,
+    version              => $version,
+    server_multithreaded => true,
+    server_storeconfigs  => $server_storeconfigs,
+    server_reports       => $server_reports,
+
   }
   if $setup_puppetboard {
     include profile_puppetmaster::puppetboard
--- manifests/puppetdb.pp
+++ manifests/puppetdb.pp
@@ -2,8 +2,20 @@
 #
 #
 class profile_puppetmaster::puppetdb {
+  yumrepo { 'postgres-repo':
+    ensure   => present,
+    baseurl  => 'https://yum.postgresql.org/9.6/redhat/rhel-8-x86_64/',
+    enabled  => 1,
+    gpgcheck => 0,
+  }
+
   # Configure puppetdb and postgres
-  class { 'puppetdb': }
+  class { 'puppetdb':
+    manage_package_repo => false,
+  }
+
   # Configure the puppetmaster to use puppetdb
-  class { 'puppetdb::master::config': }
+  class {'puppet::server::puppetdb':
+    server => $facts['networking']['fqdn'],
+  }
 }
