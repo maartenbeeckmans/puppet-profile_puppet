7293d4e3aa6d0ca7199fe01ac7c1122ef097db78
Maarten Beeckmans <maartenb@inuits.eu>
2020-10-22 20:54:58 +0200

    implement puppetboard

--- manifests/puppetboard.pp
+++ manifests/puppetboard.pp
@@ -2,5 +2,26 @@
 #
 #
 class profile_puppetmaster::puppetboard {
-  notice { 'No support for el8 yet': }
+  class { 'puppetboard':
+    manage_git          => 'latest',
+    manage_virtualenv   => 'latest',
+    reports_count       => 150,
+    offline_mode        => true,
+    default_environment => '*',
+  }
+  class { 'apache':
+    default_vhost => false,
+    purge_configs => true,
+  }
+  $wsgi = $facts['os']['family'] ? {
+    'Debian' => {package_name => "libapache2-mod-wsgi-py3", mod_path => "/usr/lib/apache2/modules/mod_wsgi.so"},
+    default  => {},
+  }
+  class { 'apache::mod::wsgi':
+    * => $wsgi,
+  }
+  class { 'puppetboard::apache::vhost':
+    vhost_name => 'localhost',
+    port       => 80,
+  }
 }
