23df92171a6b14a305ce34071747249e9ee456a3
Maarten Beeckmans <maartenb@inuits.eu>
2020-10-25 00:32:52 +0200

    manage puppetmaster heap size

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,12 +2,14 @@
 #
 #
 class profile_puppetmaster (
-  Boolean         $autosign          = false,
-  Array[String]   $autosign_entries  = [],
-  String          $version           = 'latest',
-  Boolean         $setup_puppetdb    = false,
-  Boolean         $setup_puppetboard = false,
-  Boolean         $manage_firewall   = true,
+  Boolean         $autosign                 = false,
+  Array[String]   $autosign_entries         = [],
+  String          $server_jvm_min_heap_size = '2G'
+  String          $server_jvm_max_heap_size = '2G'
+  String          $version                  = 'latest',
+  Boolean         $setup_puppetdb           = false,
+  Boolean         $setup_puppetboard        = false,
+  Boolean         $manage_firewall          = true,
 ) {
   # @TODO implement prometheus/graphite metrics
   if $setup_puppetdb {
@@ -19,15 +21,17 @@ class profile_puppetmaster (
     $server_reports = 'store'
   }
   class { 'puppet':
-    server                => true,
-    autosign              => $autosign,
-    autosign_entries      => $autosign_entries,
-    server_foreman        => false,
-    version               => $version,
-    server_multithreaded  => true,
-    server_storeconfigs   => $server_storeconfigs,
-    server_reports        => $server_reports,
-    server_external_nodes => '',
+    server                   => true,
+    autosign                 => $autosign,
+    autosign_entries         => $autosign_entries,
+    server_foreman           => false,
+    version                  => $version,
+    server_multithreaded     => true,
+    server_storeconfigs      => $server_storeconfigs,
+    server_reports           => $server_reports,
+    server_external_nodes    => '',
+    server_jvm_min_heap_size => $server_jvm_min_heap_size,
+    server_jvm_max_heap_size => $server_jvm_max_heap_size,
   }
   if $manage_firewall {
     firewall { '08140 allow puppetmaster':
