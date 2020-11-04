301440ab592624b4a5cda21c8c83f9d6dc52f7d4
Maarten Beeckmans <maartenb@inuits.eu>
2020-11-04 21:54:46 +0100

    route throug apache

--- manifests/puppetboard.pp
+++ manifests/puppetboard.pp
@@ -13,19 +13,34 @@ class profile_puppetmaster::puppetboard (
     offline_mode        => true,
     default_environment => '*',
   }
-  firewall { '08080 allow puppetboard':
-    dport  => 8080,
+  class { 'apache':
+    default_vhost => false,
+    purge_configs => true,
+  }
+  $wsgi = $facts['os']['family'] ? {
+    'Debian' => {package_name => "libapache2-mod-wsgi-py3", mod_path => "/usr/lib/apache2/modules/mod_wsgi.so"},
+    default  => {},
+  }
+  class { 'apache::mod::wsgi':
+    * => $wsgi,
+  }
+  class { 'puppetboard::apache::vhost':
+    vhost_name => 'localhost',
+    port       => 80,
+  }
+  firewall { '00080 allow puppetboard':
+    dport  => 80,
     action => 'accept',
   }
   if $manage_sd_service {
     consul::service { $sd_service_name:
       checks => [
         {
-          http     => 'http://localhost:8080',
+          http     => 'http://localhost:80',
           interval => '10s'
         }
       ],
-      port   => 8080,
+      port   => 80,
       tags   => $sd_service_tags,
     }
   }
